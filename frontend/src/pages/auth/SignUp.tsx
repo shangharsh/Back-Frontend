39f88487f4659bf58acff083c0b5589f6c974d7a
Shangharsh Adhikari <san.adh1996@gmail.com>
2023-11-02 18:04:38 +0545

    feat: SignUp page design

--- frontend/src/pages/auth/SignUp.tsx
+++ frontend/src/pages/auth/SignUp.tsx
@@ -1,7 +1,24 @@
+import { TextField } from '@mui/material'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import {Row, Col, Form} from 'react-bootstrap'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <Container>
+      <Row>
+        <Col xs={12} md={6}>
+        <h1>Sign Up</h1>
+        <Form>
+          <TextField id='name' variant='outlined' className='mb-4' required label='name' placeholder='Enter Name Here' fullWidth autoFocus/>
+          <TextField id='email' variant='outlined' className='mb-4' required label='email' placeholder='Enter Email Here' fullWidth autoFocus/>
+          <TextField id='password' variant='outlined' className='mb-4' required label='password' placeholder='Enter Password Here' fullWidth autoFocus/>
+          <TextField id='password' variant='outlined' className='mb-4' required label='password' placeholder='Enter Re-Password Here' fullWidth autoFocus/>
+          <Button type='submit'>Submit</Button>
+        </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
